67be761d9a347e75e14805e49e712dee1376ac93
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-12 01:24:27 +0800

    Delete proprietary directory

--- proprietary/bin/start_lins_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-echo LiNS > /sys/power/wake_lock
-ifconfig usb0 192.168.100.100 up
-start lins-proxy
--- proprietary/bin/stop_lins_debug.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-stop lins-proxy
-ifconfig usb0 down
-rmmod g_ether.ko
-echo LiNS > /sys/power/wake_unlock
